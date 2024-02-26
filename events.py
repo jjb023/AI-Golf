84a9cd0a0d86a3243f4290160d842a24304ab2d4
jjb023 <78971855+jjb023@users.noreply.github.com>
2024-02-26 15:16:51 +0000

    Code for extracting event id data

---
+++ events.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import csv
+
+url = 'https://feeds.datagolf.com/historical-raw-data/event-list?file_format=[ file_format ]&key=d6b6280403a3d0f3b7917387aed7'
+
+r = requests.get(url)
+data = json.loads(r.text)
+
+eventlist = []
+
+for event in data:
+    eventid = event['event_id']
+    eventname = event['event_name']
+
+    event_item = {
+        eventname,
+        eventid,
+    }   
+    eventlist.append(event_item)
+
+
+    # with open('Scores.csv', 'w', newline='') as f:
+    # writer = csv.writer(f)
+    # writer.writerow(['Player', 'Finish'])
+    # writer.writerows(ourdata)
+
+with open('EventList.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Event Name', 'Event ID'])
+    writer.writerows(eventlist)
+
+print(eventlist)
