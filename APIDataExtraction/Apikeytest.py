fd99468c90351b2a0e547fcdf319b8f7e8ff3d29
jjb023 <78971855+jjb023@users.noreply.github.com>
2024-02-26 15:20:43 +0000

    Starting to extract data from API

---
+++ APIDataExtraction/Apikeytest.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import csv
+from requests.api import head
+
+
+api_key = "d6b6280403a3d0f3b7917387aed7"
+api_url = "https://feeds.datagolf.com/historical-raw-data/rounds?tour=pga"
+
+parameters = {
+    "event_id": "5",
+    "year": "2021",
+    "file_format": "json",
+    "key": api_key
+}
+
+response = requests.get(api_url, params=parameters)
+myjson = response.json()
+ourdata = []
+
+for player in myjson['scores']:
+    listing = [player['player_name'], player['fin_text']]
+    ourdata.append(listing)
+    
+with open('Scores.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['Player', 'Finish'])
+    writer.writerows(ourdata)
