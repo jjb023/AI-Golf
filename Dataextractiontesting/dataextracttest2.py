088ff978ddc9efec0ee28b7023f16181bedd751c
jjb023 <78971855+jjb023@users.noreply.github.com>
2024-02-27 18:15:45 +0000

    Added all sg parameters to csv file

--- Dataextractiontesting/dataextracttest2.py
+++ Dataextractiontesting/dataextracttest2.py
@@ -21,55 +21,82 @@ def extract_data(api_data):
         year_played = api_data['year']
         
         scores = []
-        sg_values = []
+        sg_total = []
+        sg_app = []
+        sg_arg = []
+        sg_putt = []
         driving_distances = []
         driving_accuracies = []
         
         if 'round_1' in score:
             scores.append(score['round_1'].get('score'))
-            sg_values.append(score['round_1'].get('sg_total'))
+            sg_total.append(score['round_1'].get('sg_total'))
+            sg_app.append(score['round_1'].get('sg_app'))
+            sg_arg.append(score['round_1'].get('sg_arg'))
+            sg_putt.append(score['round_1'].get('sg_putt'))
             driving_distances.append(score['round_1'].get('driving_dist'))
             driving_accuracies.append(score['round_1'].get('driving_acc'))
         else:
             scores.append(None)
-            sg_values.append(None)
+            sg_total.append(None)
+            sg_app.append(None)
+            sg_arg.append(None)
+            sg_putt.append(None)
             driving_distances.append(None)
             driving_accuracies.append(None)
         
         if 'round_2' in score:
             scores.append(score['round_2'].get('score'))
-            sg_values.append(score['round_2'].get('sg_total'))
+            sg_total.append(score['round_2'].get('sg_total'))
+            sg_app.append(score['round_2'].get('sg_app'))
+            sg_arg.append(score['round_2'].get('sg_arg'))
+            sg_putt.append(score['round_2'].get('sg_putt'))
             driving_distances.append(score['round_2'].get('driving_dist'))
             driving_accuracies.append(score['round_2'].get('driving_acc'))
         else:
             scores.append(None)
-            sg_values.append(None)
+            sg_total.append(None)
+            sg_app.append(None)
+            sg_arg.append(None)
+            sg_putt.append(None)
             driving_distances.append(None)
             driving_accuracies.append(None)
         
         if 'round_3' in score:
             scores.append(score['round_3'].get('score'))
-            sg_values.append(score['round_3'].get('sg_total'))
+            sg_total.append(score['round_3'].get('sg_total'))
+            sg_app.append(score['round_3'].get('sg_app'))
+            sg_arg.append(score['round_3'].get('sg_arg'))
+            sg_putt.append(score['round_3'].get('sg_putt'))
             driving_distances.append(score['round_3'].get('driving_dist'))
             driving_accuracies.append(score['round_3'].get('driving_acc'))
         else:
             scores.append(None)
-            sg_values.append(None)
+            sg_total.append(None)
+            sg_app.append(None)
+            sg_arg.append(None)
+            sg_putt.append(None)
             driving_distances.append(None)
             driving_accuracies.append(None)
         
         if 'round_4' in score:
             scores.append(score['round_4'].get('score'))
-            sg_values.append(score['round_4'].get('sg_total'))
+            sg_total.append(score['round_4'].get('sg_total'))
+            sg_app.append(score['round_4'].get('sg_app'))
+            sg_arg.append(score['round_4'].get('sg_arg'))
+            sg_putt.append(score['round_4'].get('sg_putt'))
             driving_distances.append(score['round_4'].get('driving_dist'))
             driving_accuracies.append(score['round_4'].get('driving_acc'))
         else:
             scores.append(None)
-            sg_values.append(None)
+            sg_total.append(None)
+            sg_app.append(None)
+            sg_arg.append(None)
+            sg_putt.append(None)
             driving_distances.append(None)
             driving_accuracies.append(None)
         
-        extracted_data.append([player_name, course_name, year_played] + scores + sg_values +
+        extracted_data.append([player_name, course_name, year_played] + scores + sg_total + sg_app + sg_arg + sg_putt +
                               driving_distances + driving_accuracies)
     return extracted_data
 
@@ -82,11 +109,14 @@ def main(event_id, year, api_token):
         extracted_data = extract_data(api_data)
         with open('dataextractiontest2.csv', mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(['Player Name', 'Course Name', 'Year Played', 'Round 1 Score', 'Round 2 Score',
-                             'Round 3 Score', 'Round 4 Score', 'Round 1 SG', 'Round 2 SG', 'Round 3 SG',
-                             'Round 4 SG', 'Round 1 Driving Distance', 'Round 2 Driving Distance',
-                             'Round 3 Driving Distance', 'Round 4 Driving Distance', 'Round 1 Driving Accuracy',
-                             'Round 2 Driving Accuracy', 'Round 3 Driving Accuracy', 'Round 4 Driving Accuracy'])
+            writer.writerow(['Player Name', 'Course Name', 'Year Played', 
+                             'Round 1 Score', 'Round 2 Score', 'Round 3 Score', 'Round 4 Score', 
+                             'Round 1 SG Total', 'Round 2 SG Total', 'Round 3 SG Total', 'Round 4 SG Total', 
+                             'Round 1 SG App', 'Round 2 SG App', 'Round 3 SG App', 'Round 4 SG App', 
+                             'Round 1 SG Arg', 'Round 2 SG Arg', 'Round 3 SG Arg', 'Round 4 SG Arg',
+                             'Round 1 SG Putt', 'Round 2 SG Putt', 'Round 3 SG Putt', 'Round 4 SG Putt',
+                             'Round 1 Driving Distance', 'Round 2 Driving Distance', 'Round 3 Driving Distance', 'Round 4 Driving Distance', 
+                             'Round 1 Driving Accuracy', 'Round 2 Driving Accuracy', 'Round 3 Driving Accuracy', 'Round 4 Driving Accuracy'])
             for data_row in extracted_data:
                 writer.writerow(data_row)
         print("CSV file created successfully!")
